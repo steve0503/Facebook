e9b6aa768cc2ece3e6c3b8ddbb6aed58645800b1
steve0503 <leehr76@gmail.com>
2014-01-21 11:33:32 +0900

    Facebook
    OK

--- Facebook/ViewController.m
+++ Facebook/ViewController.m
@@ -7,12 +7,51 @@
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+-(IBAction)showSocialComposer:(id)sender{
+    
+    NSString *service = SLServiceTypeFacebook;
+    
+    if([SLComposeViewController isAvailableForServiceType:service]){
+        
+        SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:service];
+        
+        
+        UIImage *image = [UIImage imageNamed:@"iu1.jpg"];
+        
+        [composer addImage:image];
+        
+        [composer setInitialText:@"소셜 프레임워크를 이용한 글 쓰기 테스트"];
+        
+        composer.completionHandler = ^(SLComposeViewControllerResult result){
+            
+            if (SLComposeViewControllerResultDone == result) {
+                NSLog(@"글 작성 완료");
+            }
+            else{
+                
+                
+                NSLog(@"글 작성 취소");
+                
+                
+            }
+            
+            
+        };
+        
+        [self presentViewController:composer animated:YES completion:nil];
+    }
+    
+    
+    
+}
 
 - (void)viewDidLoad
 {
